0ae1c339b365a1a8f3f667c78d5371c2f2edcffb
Suraj Shukla <suraj@zengroup.co.in>
2024-06-25 14:27:49 +0530

    [ADD]: Doubly Linked List Implementation

--- LinkedList/DoublyLinkedList.js
+++ LinkedList/DoublyLinkedList.js
@@ -1,38 +1,35 @@
 // piece of data - val
 // reference to the next node - next
+// reference to the pren node - prev
 
 class Node {
     constructor(val) {
         this.val = val;
         this.next = null;
+        this.prev = null;
     }
 }
 
-class SinglyLinkedList {
+class DoublyLinkedList {
     constructor() {
         this.length = 0;
         this.head = null;
         this.tail = null;
     }
 
-    /**
-     *
-     * @param {*} val
-     * @returns SinglyLinkedList
-     * @description adds the value to the end of the list
-     */
-
     push(val) {
         // 1. Create a node
-        // 2. if there is no head assign the node as head and tail.
-        // 3. if there is a tail, tail.next = node and assign node as tail
-        // 4. increase length of list
-        // 5. return list
+        // 2. assign new node prev as tail
+        // 3. if there is no head assign the node as head and tail.
+        // 4. if there is a tail, tail.next = node and assign node as tail
+        // 5. increase length of list
+        // 6. return list
         const node = new Node(val);
         if (!this.head) {
             this.head = node;
-            this.tail = this.head;
+            this.tail = node;
         } else {
+            node.prev = this.tail;
             this.tail.next = node;
             this.tail = node;
         }
@@ -47,30 +44,32 @@ class SinglyLinkedList {
      */
     pop() {
         if (!this.head) return;
-        let current = this.head;
-        let newTail = current;
-        while (current.next) {
-            newTail = current;
-            current = current.next;
-        }
-        this.tail = newTail;
-        this.tail.next = null;
-        this.length--;
-        if (this.length === 0) {
+        if (this.length === 1) {
             this.head = null;
             this.tail = null;
+        } else {
+            const newTail = this.tail.prev;
+            this.tail.prev = null;
+            newTail.next = null;
+            this.tail = newTail;
         }
-        return current;
+        this.length--;
+        return this.tail;
     }
 
     shift() {
         if (!this.head) return;
         const oldHead = this.head;
-        this.head = oldHead.next;
-        this.length--;
-        if (this.length === 0) {
+        if (this.length === 1) {
             this.tail = null;
+            this.head = null;
+        } else {
+            this.head = oldHead.next;
+            oldHead.next = null;
+            this.head.prev = null;
         }
+
+        this.length--;
         return oldHead;
     }
 
@@ -80,6 +79,7 @@ class SinglyLinkedList {
             this.head = newNode;
             this.tail = newNode;
         } else {
+            this.head.prev = newNode;
             newNode.next = this.head;
             this.head = newNode;
         }
@@ -87,54 +87,54 @@ class SinglyLinkedList {
         return this;
     }
 
-    traverse() {
-        let current = this.head;
-        while (current) {
-            console.log(current.val);
-            current = current.next;
-        }
-    }
-
     get(index) {
-        if (this.length === 0 || index > this.length || index < 0) return;
-        let current = this.head;
-        let count = 0;
+        if (index >= this.length || index < 0) return;
+        const isFrontNear = index <= Math.floor(this.length / 2);
+        let current = isFrontNear ? this.head : this.tail;
+        let count = isFrontNear ? 0 : this.length - 1;
         while (count !== index) {
-            current = current.next;
-            count++;
+            if (isFrontNear) {
+                current = current.next;
+                count++;
+            } else {
+                current = current.prev;
+                count--;
+            }
         }
 
         return current;
     }
 
     set(index, val) {
-        let found = this.get(index);
-        if (found) {
-            found.val = val;
+        const node = this.get(index);
+        if (node) {
+            node.val = val;
             return true;
         }
         return false;
     }
 
     insert(index, val) {
-        if (this.length === 0 || index > this.length || index < 0) return false;
+        if (index < 0 || index > this.length) return;
         if (index === this.length) {
             return !!this.push(val);
         }
         if (index === 0) {
             return !!this.unshift(val);
         }
-        const newNode = new Node(val);
-        let prevNode = this.get(index - 1);
-        let nextNode = prevNode.next;
-        prevNode.next = newNode;
-        newNode.next = nextNode;
+
+        const node = new Node(val);
+        const prevNode = this.get(index - 1);
+        node.next = prevNode.next;
+        node.prev = prevNode;
+        prevNode.next = node;
+        node.next.prev = node;
         this.length++;
         return true;
     }
 
     remove(index) {
-        if (this.length === 0 || index > this.length || index < 0) return false;
+        if (index < 0 || index > this.length) return;
         if (index === this.length - 1) {
             return this.pop();
         }
@@ -142,45 +142,38 @@ class SinglyLinkedList {
             return this.shift();
         }
 
-        let prevNode = this.get(index - 1);
-        const current = prevNode.next;
-        prevNode.next = current.next;
+        const node = this.get(index);
+        const prevNode = node.prev;
+        const nextNode = node.next;
+        prevNode.next = nextNode;
+        nextNode.prev = prevNode;
+        node.next = null;
+        node.prev = null;
         this.length--;
-        return current;
+        return node;
     }
 
     reverse() {
         let node = this.head;
         this.head = this.tail;
         this.tail = node;
-        let next;
+        let next = null;
         let prev = null;
         for (let i = 0; i < this.length; i++) {
             next = node.next;
             node.next = prev;
+            node.prev = next;
             prev = node;
             node = next;
         }
-
-        return this;
     }
 }
 
-const list = new SinglyLinkedList();
+const list = new DoublyLinkedList();
 list.push(1);
 list.push(2);
 list.push(3);
 
-list.reverse();
-list.traverse();
-
 console.log(list);
-
-/**
- * Insertion -  O(1)
- * Removal -
- *      1. at Begennong or at end O(1)
- *      2. anywhere between O(N)
- * Searching - O(N)
- * Access - O(N)
- */
+console.log(list.reverse());
+console.log(list);
